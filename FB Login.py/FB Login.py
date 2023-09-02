c69c59e5e79b74b5c1fee41c63ed375535f3bdea
eeve1025 <126946790+eeve1025@users.noreply.github.com>
2023-09-02 08:20:20 +0000

    1

---
+++ FB Login.py/FB Login.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+services=Service(executable_path="C:\\Users\\user\\Desktop\\big_data\\chromedriver.exe")
+driver=webdriver.Chrome(service=services)
+driver.get("https://www.facebook.com")
+
+driver.find_element(By.CSS_SELECTOR,'input[name="email"]').send_keys("************@yahoo.com.tw")
+driver.find_element(By.CSS_SELECTOR,'input[name="pass"]').send_keys("***********")
+driver.find_element(By.CSS_SELECTOR,'button[name="login"]').click()
